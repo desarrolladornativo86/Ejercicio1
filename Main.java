409d04cab6bd0671ca70f94bb1448d76a02333a2
desarrolladornativo86 <106443583+desarrolladornativo86@users.noreply.github.com>
2022-07-10 13:11:34 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+        int numero = 9999;
+        double precio = 10.50;
+        boolean check = false;
+        long valor = 88;
+        System.out.println("String: " + cadena);
+        System.out.println("Int: " + numero);
+        System.out.println("Double: " + precio);
+        System.out.println("Boolean: " + check);
+        System.out.println("Long: " + valor);
+
+    }
+}
